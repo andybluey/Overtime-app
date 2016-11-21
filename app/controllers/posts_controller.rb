d0c78fbc177510657820159271df329f43c4d545
Andrew Knight <andrewjaknight@gmail.com>
2016-11-22 09:08:15 +1100

    Implemented new index query for showing users posts they created and hiding others

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   def new
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
   def update
     authorize @post
-    
+
     if @post.update(post_params)
       redirect_to @post, notice: 'Your post was edited successfully'
     else
